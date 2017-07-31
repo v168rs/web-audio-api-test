d4143a18f178b0d8b0f33b086cdcfebf839c5309
Ryan Lee <v168rs@gmail.com>
2017-07-31 10:55:40 -0700

    Marginally better documentation for geo-audio-prox
    Also changed some of the webpage colors and added a drag and drop framework

--- js/geo-audio-prox-html5.js
+++ js/geo-audio-prox-html5.js
@@ -3,6 +3,10 @@
 //Requires Cesium.js
 //National anthems (soundfile, lat, long, flagicon) maybe volume too (do something cool like volume proportional to GDP)
 
+//TODO: Use JSON instead of this giant array to store URLs, lat/lons, etc
+
+			  
+//Massive list of audio samples that should probably be moved somewhere else
 var geo_audio_samples = [[
         ["snd/usa.mp3", 38.8977, -77.0365, "img/us.png"],     //http://www.music.army.mil/music/nationalanthem/
         ["snd/canada.mp3", 45.421530, -75.697193, "img/ca.png"],
@@ -187,8 +191,10 @@ var geo_audio_samples = [[
 ];
 //Flags are kept at https://drive.google.com/open?id=0B6_a4sq0zv4FT0N2TUNwb3ZqNnc
 
+//Audio attributes that vary depending on sound set: reverb impulse, inner cone (no volume reduction), outer cone
+var geo_audio_attributes = [["snd/imp/impulse.wav", 10, 140], ["snd/imp/impulse3.wav", 90, 180]];
 
-//Takes a path/URL to the sample and a function that receives the arraybuffer data as a parameter. Don't call this multiple times for multiple samples. Instead use multiple_xhr
+//Takes a path/URL to the sample and a callback that receives the arraybuffer data as a parameter. MOSTLY DEPRECATED AND ONLY USED FOR CONVOLUTION REVERB.
 function load_sample(sample, buffer_receiver) {
     var getSound = new XMLHttpRequest();
     getSound.open("GET", sample, true);
@@ -201,20 +207,21 @@ function load_sample(sample, buffer_receiver) {
     getSound.send();
 }
 
-var geo_buses = [], //Array [] which holds arrays of [panners (for spatialization) and AudioBufferSourceNodes]. Panners are initialized in create_samples_with_loc.
+var geo_buses = [], //Array [] which holds arrays of [panners and AudioBufferSourceNodes]. Panners are initialized in create_samples_with_loc.
     cur_audio_selector = 0, //Determines the set of sounds being played ie Anthems, Ambient
-    context, //AudioContext
-    convolver, //Fancy reverb for sense of space
-	filter, //Filter (currently being modulated by color)
+    context, //AudioContext for the page
+    convolver, //Fancy reverb for sense of space (initialized in geo_init)
+	filter, //Doesn't exist anymore
     master_gain, //Master volume
     listening_nodes = 0; //Number of nodes being listened to help cap the number of nodes that can be activated, updated by find_prox_nodes
 
-var data_canvas_context;
+var data_canvas_context; //Doesn't do anything anymore
 
 function map(val, min1, max1, min2, max2) {
     return (val - min1) / (max1 - min1) * (max2 - min2) + min2;
-} //from p5js
+} //from p5js. Maps a value within one range to another.
 
+//Initializes audio context, master gain, convolution reverb.
 function geo_init(impulse = "snd/imp/impulse.wav") {
     context = new AudioContext();
     master_gain = context.createGain();
@@ -224,7 +231,7 @@ function geo_init(impulse = "snd/imp/impulse.wav") {
     load_sample(impulse, function(b) {
         convolver.buffer = b;
     });
-	/*
+	/* //Deprecated
 	filter = context.createBiquadFilter(); //Modulation test
 	filter.type = "lowpass";
 	filter.frequency.value = 100;
@@ -236,6 +243,7 @@ function geo_init(impulse = "snd/imp/impulse.wav") {
     
 }
 
+/* //Deprecated
 function img_update(img_src="img/data/lstd_01_gs.PNG") {
 	var data_img = new Image();
 	if(!data_canvas_context) {
@@ -251,13 +259,16 @@ function img_update(img_src="img/data/lstd_01_gs.PNG") {
 }
 
 img_update();
+*/
 
+//Changes reverb
 function change_impulse(impulse = "snd/imp/impulse.wav") {
     load_sample(impulse, function(b) {
         convolver.buffer = b;
     });
 }
 
+// Math helpers
 function deg2rad(x) {
     return x/180 * Math.PI;
 }
@@ -266,71 +277,63 @@ function rad2deg(x) {
 	return (x/Math.PI * 180) % 360;
 }
 
+//cached variables explained below
 var ind_arr = [],
-    url_arr = [],
     dist_arr = [],
-    rej_arr = [];
-//4000 km?
-var cur_stat = 0;
-
+    rej_arr = [],
+	cur_stat = 0; //Deprecated
+			  
+//Finds nodes within proximity of the viewer based on their latitude and longitude. Range is in m, audio_selector determines the sound set, max_nodes determines the maximum number of nodes.
 function find_prox_nodes(range = 2000000, audio_selector = 0, lat, long, max_nodes = 7) {
 	var geo6c = set_samples_loc(null, lat, long);
-    ind_arr = [];
-    url_arr = [];
-    dist_arr = [];
-    rej_arr = [];
+    ind_arr = []; //Array containing indices of sounds within range (as within geo_audio_samples[audio_selector])
+    dist_arr = []; //Array containing distances of sounds so we can sort them
+    rej_arr = []; //Array containing sounds current playing that we will shut off because they are outside of range.
     geo_audio_samples[audio_selector].forEach(function(arr, index){
         var geo6 = set_samples_loc(arr);
         var dist = Math.sqrt(Math.pow((geo6[0] - geo6c[0]), 2) + Math.pow((geo6[1] - geo6c[1]), 2) + Math.pow((geo6[2] - geo6c[2]), 2));
         if(dist < range) {
             ind_arr.push(index);
-            url_arr.push(arr[0]);
             dist_arr.push(dist);
         }
-        else {
+        else if(geo_buses[index] && (geo_buses[index][0] instanceof MediaElementAudioSourceNode)) { //Look for nodes that are playing outside of range...
             rej_arr.push(index);
         }
     });
-    rej_arr.forEach(function(index) {
-        if(geo_buses[index] && (geo_buses[index][0] instanceof MediaElementAudioSourceNode)) {
-            geo_buses[index][0].disconnect();
-            if(!geo_buses[index][0].paused) {try {geo_buses[index][0].mediaElement.pause(); } catch(e) {} }
-            geo_buses[index][0] = null;
-            listening_nodes -= 1;
-        }
+    rej_arr.forEach(function(index) { //Pause and delete nodes out of range.
+        geo_buses[index][0].disconnect();
+        if(!geo_buses[index][0].paused) {try {geo_buses[index][0].mediaElement.pause(); } catch(e) {} }
+        geo_buses[index][0] = null;
+        listening_nodes -= 1;
     });
     //Sort by proximity (closest first) - Should prevent zooming on a country only to discover you can't hear it
     var arr_keys = Object.keys(dist_arr);
     arr_keys.sort(function(a, b){return dist_arr[a] < dist_arr[b]});
-    url_arr = arr_keys.map(function(index){
-       return url_arr[index];
-    });
     ind_arr = arr_keys.map(function(index){
        return ind_arr[index];
     });
     //Then slice to fit
-    url_arr = url_arr.slice(0, max_nodes-listening_nodes);
     ind_arr = ind_arr.slice(0, max_nodes-listening_nodes);
     
-    if(url_arr[0] && (listening_nodes < max_nodes)) {
-        //Play it HTML5 style
-        url_arr.forEach(function(url, index){
-            if((geo_buses[ind_arr[index]][0] === undefined) || (geo_buses[ind_arr[index]][0] === null)) {
-                var audio_source = context.createMediaElementSource(new Audio(url));
-                audio_source.mediaElement.loop = true;
-                audio_source.mediaElement.play();
-                geo_buses[ind_arr[index]][0] = audio_source;
-                audio_source.connect(geo_buses[ind_arr[index]][1]);
+    if(ind_arr[0] && (listening_nodes < max_nodes)) {
+        ind_arr.forEach(function(index){
+            if((geo_buses[index][0] === undefined) || (geo_buses[index][0] === null)) { //If there isn't already a sound there
+                var audio_source = context.createMediaElementSource(new Audio(geo_audio_samples[audio_selector][index][0])); //Creates an HTML5 audio element that points to a specific URL.
+                audio_source.mediaElement.loop = true; //Why not a regular WebAudioAPI AudioBufferSourceNode? Because for some reason those leak memory like crazy.
+                audio_source.mediaElement.play(); //It has something to do with array buffers.
+                geo_buses[index][0] = audio_source;
+                audio_source.connect(geo_buses[index][1]);
             }
         });
         //99999 is a null value. lat/lon is lat. but can also be expressed by polar to equirectangular:
     }
+	/* //Deprecated
 	var y = Math.round((90 - rad2deg(lat)) * 10)  - data_canvas_context.canvas.offsetLeft; //1800
 	var x = Math.round((rad2deg(long) + 180) * 10)  - data_canvas_context.canvas.offsetTop;
 	var color = data_canvas_context.getImageData(x, y, 1, 1).data;
 	if(color) {
 		cur_stat = Math.max(color[0]/255, color[1]/255, color[2]/255);
-	}
+	} */
 } //0 to 1
 
 //A function used for calculating the reference values for audio position and orientation. Returns array of 6 numbers with cartesian position and surface normal - I don't know how the math works
@@ -357,14 +360,16 @@ function set_samples_loc(arr = null, lat = 0, long = 0){
     return ret;
 }
 
+//Unified function for setting position and orientation of the AudioContext's listener
 function set_listener_loc(x, y, z, fx, fy, fz, ux, uy, uz) {
     context.listener.setPosition(x, y, z);
     context.listener.setOrientation(fx, fy, fz, ux, uy, uz);
 }
-
+			  
 geo_init();
 
-var viewer = new Cesium.Viewer('cesiumContainer');
+//Cesium stuff
+var viewer = new Cesium.Viewer('cesiumContainer', {"sceneModePicker" : false, "timeline" : false}); //Scenemodepicker makes the camera position weird in some modes so we got rid of it
 var camera = viewer.camera;
 camera.changed.addEventListener(function() {
     set_listener_loc(camera.position["x"], camera.position["y"], camera.position["z"], camera.direction["x"], camera.direction["y"], camera.direction["z"], camera.up["x"], camera.up["y"], camera.up["z"]);
@@ -372,7 +377,7 @@ camera.changed.addEventListener(function() {
 	//filter.frequency.linearRampToValueAtTime(map(cur_stat, 0, 1, 100, 6000), context.currentTime+1);
                                            });
 
-//Just for testing
+//Stress testing function
 function repeat_listener_rand_loc(rep = 100) {
     var i,
         rand = Math.random(),
@@ -382,8 +387,7 @@ function repeat_listener_rand_loc(rep = 100) {
     }
 }
 
-
-//Placing icons in Cesium (national flags)
+//Placing icons in Cesium
 function place_billboard(bx, by, bz, b_img) {
 	var csnfs = new Cesium.NearFarScalar(1e7, 1.0, 5.0e7, 0.0);
     viewer.entities.add({
@@ -395,18 +399,17 @@ function place_billboard(bx, by, bz, b_img) {
     });
 }
 
-//Initializes panners at preset locations with orientation away from the Earth's surface. Doesn't actually create samples anymore.
+//Initializes panners at preset locations with orientation away from the Earth's surface. Doesn't actually create samples.
 function create_samples_with_loc(audio_selector = 0, cone_inner = 10, cone_outer = 140){
     geo_audio_samples[audio_selector].forEach(function(arr){
         var geo_bus = [];
         var panner = context.createPanner();
         panner.panningModel = "HRTF"; //More realistic and supports 3D panning but computationally costly. Cheaper option is "equalpower" which is just L/R.
-        //Positioning
         
-        //cesium.js
+		//Positioning
         var geo6 = set_samples_loc(arr);
         panner.setPosition(geo6[0], geo6[1], geo6[2]);
-        panner.refDistance = 700;
+        panner.refDistance = 700; //Base distance unit for panner calculations
         
         //Orientation
         panner.setOrientation(geo6[3], geo6[4], geo6[5]);
@@ -423,7 +426,7 @@ function create_samples_with_loc(audio_selector = 0, cone_inner = 10, cone_outer
         if (arr[3]) {
             place_billboard(geo6[0], geo6[1], geo6[2], arr[3]);
         }
-        else {
+        else { //Defaults to a picture of a green circle
             place_billboard(geo6[0], geo6[1], geo6[2], "img/xph.png");
         }
     });
@@ -442,9 +445,8 @@ function change_imagery_layer(iurl = "img/data/lstd_01_c.PNG") {
 }
 */
 			  
+//Function to switch which set of samples you're using
 function change_sample_bank(audio_selector = 0) {
-    //impulse, cone_inner, cone_outer
-    var geo_audio_attributes = [["snd/imp/impulse.wav", 10, 140], ["snd/imp/impulse3.wav", 90, 180]];
     //clear geo_buses
     cur_audio_selector = audio_selector;
     geo_buses.forEach(function(bus) {
@@ -466,7 +468,8 @@ function change_sample_bank(audio_selector = 0) {
         change_impulse("snd/imp/impulse2.wav");
     }
 }
-/*
+
+			  /*
 var slider = document.getElementById("data_pic_slider");
 slider.oninput = function() {
 
@@ -492,5 +495,49 @@ document.getElementById("unmute_btn").addEventListener("click", function() {
 	document.getElementById("unmute_btn").disabled = true;
 	document.getElementById("mute_btn").disabled = false;
 });
+
+document.getElementById("cesiumContainer").ondrop = function(event) {
+	this.style.opacity = 1;
+	event.stopPropagation();
+	event.preventDefault();
+	var dt = event.dataTransfer;
+	if(dt.items) {
+		var i = 0;
+		for (i = 0; i <= dt.items.length - 1; i++) {
+			if(dt.items[i] && dt.items[i].kind == "file") {
+				var f = dt.items[i].getAsFile();
+				if(f.type == "audio/mp3") {
+					var fr = new FileReader();
+					fr.readAsDataURL(f);
+					fr.onload = function(file) {
+						//audio is stored in fr.result but this may not be the best way to go about it
+					}
+					//geo_audio_samples[3].push(f)
+				}
+			}
+			else {
+				dt.items[i].getAsString(function(str) {
+					console.log(str);
+				});
+			}
+		}
+		
+	}
+}
+
+document.getElementById("cesiumContainer").ondragenter = function(event) {
+	this.style.opacity = 0.4;
+}
+
+document.getElementById("cesiumContainer").ondragleave = function(event) {
+	this.style.opacity = 1;
+}
+
+document.getElementById("cesiumContainer").ondragover = function(event) {
+	event.preventDefault();
+	event.dataTransfer.effectAllowed = "move";
+	event.dataTransfer.dropEffect = "move";
+	}
 			  
 			 }());
+
